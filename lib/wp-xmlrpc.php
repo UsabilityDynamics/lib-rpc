d8e92e83965c214f6fab6393d9743146ee4dfb79
Anton Korotkov <anton.korotkov@usabilitydynamics.com>
2013-11-04 15:17:14 +0200

    WIP

--- lib/wp-xmlrpc.php
+++ lib/wp-xmlrpc.php
@@ -24,8 +24,13 @@ namespace UsabilityDynamics {
      * @param type $timeout
      * @author korotkov@ud
      */
-    function __construct($server, $path = false, $port = 80, $timeout = 15) {
+    function __construct($server, $key, $headers = array(), $path = false, $port = 80, $timeout = 15) {
       parent::__construct( $server, $path, $port, $timeout );
+
+      /**
+       * Encryption key
+       */
+      $this->ecryption_key = $key;
 
       /**
        * Custom useragent
@@ -35,8 +40,142 @@ namespace UsabilityDynamics {
       /**
        * Custom headers
        */
-      $this->headers = array();
+      $this->headers = $headers;
 
+      /**
+       * Enable debug
+       */
+      $this->debug = 1;
+
+    }
+
+    /**
+     *
+     * @return boolean
+     */
+    function query() {
+
+      /**
+       * All request args
+       */
+      $args = func_get_args();
+
+      /**
+       * Get method stored on first place
+       */
+      $method = array_shift($args);
+
+      /**
+       * Hash request data
+       */
+      $secure_args = array(
+        array(
+          trim(
+            base64_encode(
+              mcrypt_encrypt(
+                MCRYPT_RIJNDAEL_256,
+                md5( $this->ecryption_key ),
+                json_encode( $args[0] ),
+                MCRYPT_MODE_ECB,
+                mcrypt_create_iv(
+                  mcrypt_get_iv_size(
+                    MCRYPT_RIJNDAEL_256,
+                    MCRYPT_MODE_ECB
+                  ),
+                  MCRYPT_RAND
+                )
+              )
+            )
+          )
+        )
+      );
+
+      /**
+       * Build request
+       */
+      $request = new \IXR_Request($method, $secure_args);
+      $length = $request->getLength();
+      $xml = $request->getXml();
+      $r = "\r\n";
+      $request = "POST {$this->path} HTTP/1.0$r";
+      $this->headers['Host'] = $this->server;
+      $this->headers['Content-Type'] = 'text/xml';
+      $this->headers['User-Agent'] = $this->useragent;
+      $this->headers['Content-Length'] = $length;
+      foreach ($this->headers as $header => $value) {
+        $request .= "{$header}: {$value}{$r}";
+      }
+      $request .= $r;
+      $request .= $xml;
+
+      /**
+       * Debug step
+       */
+      if ($this->debug) {
+        echo '<pre class="ixr_request">' . htmlspecialchars($request) . "\n</pre>\n\n";
+      }
+
+      /**
+       * Send
+       */
+      if ($this->timeout) {
+        $fp = @fsockopen($this->server, $this->port, $errno, $errstr, $this->timeout);
+      } else {
+        $fp = @fsockopen($this->server, $this->port, $errno, $errstr);
+      }
+      if (!$fp) {
+        $this->error = new \IXR_Error(-32300, 'transport error - could not open socket');
+        return false;
+      }
+      fputs($fp, $request);
+      $contents = '';
+      $debugContents = '';
+      $gotFirstLine = false;
+      $gettingHeaders = true;
+      while (!feof($fp)) {
+        $line = fgets($fp, 4096);
+        if (!$gotFirstLine) {
+          if (strstr($line, '200') === false) {
+            $this->error = new \IXR_Error(-32300, 'transport error - HTTP status code was not 200');
+            return false;
+          }
+          $gotFirstLine = true;
+        }
+        if (trim($line) == '') {
+          $gettingHeaders = false;
+        }
+        if (!$gettingHeaders) {
+          $contents .= $line;
+        }
+        if ($this->debug) {
+          $debugContents .= $line;
+        }
+      }
+      if ($this->debug) {
+        echo '<pre class="ixr_response">' . htmlspecialchars($debugContents) . "\n</pre>\n\n";
+      }
+
+      /**
+       * Now parse what we've got back
+       */
+      $this->message = new \IXR_Message($contents);
+      if (!$this->message->parse()) {
+        /**
+         * XML is bad
+         */
+        $this->error = new \IXR_Error(-32700, 'parse error. not well formed');
+        return false;
+      }
+
+      /**
+       * Is the message a fault?
+       */
+      if ($this->message->messageType == 'fault') {
+        $this->error = new \IXR_Error($this->message->faultCode, $this->message->faultString);
+        return false;
+      }
+
+      return true;
     }
 
   }
@@ -60,12 +199,20 @@ namespace UsabilityDynamics {
     protected $namespace;
 
     /**
+     * Encryption key
+     * @var type
+     */
+    public $key;
+
+    /**
      * Construct
      * @param type $namespace
      */
-    function __construct($namespace = 'ud') {
+    function __construct($key, $namespace = 'ud') {
 
       $this->namespace = $namespace;
+
+      $this->key = $key;
 
       $reflector = new \ReflectionClass($this);
 
@@ -182,8 +329,24 @@ namespace UsabilityDynamics {
      * @param type $last_name
      * @return type
      */
-    public function say_hello($first_name, $last_name) {
-      return 'Hello ' . $first_name . ' ' . $last_name;
+    public function test( $request_data ) {
+      return json_decode(
+        trim(
+          mcrypt_decrypt(
+            MCRYPT_RIJNDAEL_256,
+            md5( $this->key ),
+            base64_decode($request_data),
+            MCRYPT_MODE_ECB,
+            mcrypt_create_iv(
+              mcrypt_get_iv_size(
+                MCRYPT_RIJNDAEL_256,
+                MCRYPT_MODE_ECB
+              ),
+              MCRYPT_RAND
+            )
+          )
+        )
+      );
     }
 
   }
